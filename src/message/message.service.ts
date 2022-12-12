f3b3c3b1419f0270fffe57274a44cea415ba8a2f
nhathoagn <nguyennhathoang35@gmail.com>
2022-12-12 16:27:05 +0700

    feat: removeMessage

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -5,18 +5,42 @@ import { CreatedMessageDTO } from './dto/createMessage.dto';
 import { MessageEntity } from './entity/message.entity';
 import { Socket } from 'socket.io';
 import { RoomEntity } from 'src/rooms/entity/room.entity';
+import { RemoveMessage } from './dto/removeMessage.dto';
 
 @Injectable()
 export class MessageService {
   constructor(
     @InjectRepository(MessageEntity)
-    private messageService: Repository<MessageEntity>,
+    private messageRopository: Repository<MessageEntity>,
   ) {}
   async create(message: CreatedMessageDTO, socket: Socket, roomId: RoomEntity) {
-    return this.messageService.save({
+    return this.messageRopository.save({
       ...message,
       user: socket.data.user,
       room: roomId,
     });
+  }
+
+  async findMess(messageData: RemoveMessage) {
+    return await this.messageRopository.find({
+      where: {
+        user: { id: messageData.userId },
+        room: { id: messageData.roomId },
+        text: messageData.message,
+      },
+    });
+  }
+
+  findById(messageId: number) {
+    return this.messageRopository.findOne({
+      where: {
+        id: messageId,
+      },
+    });
+  }
+
+  async removeMessage(messageData: MessageEntity) {
+    const message = await this.findById(messageData.id);
+    return this.messageRopository.save({ ...message, isShow: false });
   }
 }
